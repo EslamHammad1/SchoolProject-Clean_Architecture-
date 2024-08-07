538c754bf8c18031d1b4caf95816c9ef27beeb2b
Eslam Hammad <eslamhammadxz13@gimal.com>
2024-08-07 17:46:48 +0300

    Add project files.

---
+++ SchoolProject.Data/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+            DepartmentSubjects = new HashSet<DepartmentSubject>();
+        }
+        public int dId { get; set; }
+        [StringLength(500)]
+        public string dName { get; set; }
+        public virtual ICollection<Student> Students { get;}
+        public virtual ICollection<DepartmentSubject> DepartmentSubjects { get; set; }
+
+    }
+}
---
+++ SchoolProject.Data/Entities/Subjects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public class Subjects
+    {
+        public Subjects()
+        {
+
+        }
+        public int subId { get; set; }
+        [StringLength(500)]
+        public string SubjectName { get; set; }
+        public DateTime Period { get; set; }
+    }
+}
